0d639486ea4395646635f6308e0bb26796adb4ab
Gatzmoz <gilangminecraft21@gmail.com>
2024-08-30 20:46:04 +0700

    feat: editing landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,17 @@ import HeroParallaxDemo from '@/components/example/hero-parallax-demo';
 
 export default function Home() {
   return (
-    <main className="flex overflow-hidden min-h-screen flex-col items-center justify-between p-24">
-      <HeroParallaxDemo />
-      <AppleCardsCarouselDemo />
+    <main className="flex overflow-hidden min-h-screen flex-col items-center justify-between">
+      <div className="flex flex-col items-center justify-center h-[100vh]">
+        <Image
+          src={'/assets/images/logo/gatzmoz.png'}
+          alt={'Gatmoz'}
+          width={200}
+          height={200}
+          quality={100}
+        />
+        <p className="text-2xl font-light">Gatzmoz's Portfolio</p>
+      </div>
     </main>
   );
 }
---
+++ src/components/minecomponents/Brand.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Brand() {
+  return (
+    <div>
+      <h1 className="text-2xl">GPort</h1>
+    </div>
+  );
+}
